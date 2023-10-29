b1727e79d5f90c8ff31363906dc85f116bf77786
Théo Pomies <theo.pomies@outlook.com>
2023-10-29 13:06:35 +0100

    ADD: docker, compose, pg, orm

---
+++ src/db/index.ts
@@ -0,0 +1,20 @@
+// import * as schema from "./schema";
+import { drizzle } from "drizzle-orm/postgres-js";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+import postgres from "postgres";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+const sql = postgres(connectionString, { max: 1 });
+export const db = drizzle(sql, {
+  /* schema */
+});
+
+await migrate(db, { migrationsFolder: "drizzle" });
