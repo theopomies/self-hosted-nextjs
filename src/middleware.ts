c4e750153e82d54c8ce31182e45997b33fa2b89b
Théo Pomies <theo.pomies@outlook.com>
2023-10-30 17:46:37 +0100

    ADD: Protected route via middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,23 @@
 import NextAuth from "next-auth";
 import { authConfig } from "@/lib/auth/auth.config";
+import { NextResponse } from "next/server";
 
-// Necessary if I want a middleware because DrizzleAdapter can only run on the server
-export default NextAuth(authConfig).auth;
+// Necessary if I want a middleware because Database Adapter can only run on the server
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const isProtectedRoute = req.nextUrl.pathname.startsWith("/protected");
+
+  if (isProtectedRoute) {
+    if (req.auth) {
+      console.log("Authenticated");
+      return;
+    }
+
+    console.log("Not authenticated");
+    return NextResponse.redirect(new URL("/api/auth/signin", req.url));
+  }
+});
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
